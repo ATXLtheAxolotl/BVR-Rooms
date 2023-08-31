58c53e67cfae86da68a49c299ca615fdd2e3a380
ATXLtheAxolotl <flashtube107@gmail.com>
2023-08-30 21:55:41 -0400

    fix(): restart proof

--- index.ts
+++ index.ts
@@ -1,17 +1,26 @@
+import express from "express";
 import path from "path";
 import fs from 'fs';
 
+const positionPath = __dirname + "/position";
+if(!fs.existsSync(positionPath))
+    fs.writeFileSync(positionPath, "0");
+
+const startRead = Number(fs.readFileSync(positionPath, { encoding: "utf8" }));
 const filePath = path.resolve("./data/rooms.json");
+const start = isNaN(startRead) ? 0 : startRead;
+const app = express();
 
 function getStream() {
-    const stream = fs.createReadStream(filePath, { encoding: 'utf8', highWaterMark: 1 });
-    return stream;
+    return fs
+        .createReadStream(filePath, { encoding: 'utf8', highWaterMark: 1, start });
 };
 
 const stream = getStream();
-
-let room: string;
+let position: number = 0;
 let bricks: string;
+let room: string;
+
 stream.on('data', (data) => {
     switch(data) {
         case " ":
@@ -26,23 +35,30 @@ stream.on('data', (data) => {
             const split = bricks.split('[');
             split.shift();
 
-            fs.writeFileSync(path.resolve(`${directory}/${room}.json`), JSON.stringify(JSON.parse(`[${split.join("")}]`), null, 2));
+            fs.writeFile(path.resolve(`${directory}/${room}.json`), JSON.stringify(JSON.parse(`[${split.join("")}]`), null, 2), () => {});
 
             bricks = "";
             bricks += data;
             break;
         case "[":
+            fs.writeFileSync(__dirname + "/position", position.toString());
             room = "";
 
             for(let i = bricks.length - 3; i >= 0; i--) {
                 if(isNaN(Number(bricks[i])))
                     break;
+
                 room += bricks[i];
             }
+
             console.log()
             bricks += data;
             break;
         default:
             bricks += data;
     }
+
+    position += data.length;
 })
+
+app.get("/", (_, res) => res.send(`${position} bytes`));
